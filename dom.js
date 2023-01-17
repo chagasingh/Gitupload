1c29b9175775a67bdeeb0701cfc70bc30e4b80d8
chagan singh <chagansingh9876@gmail.com>
2023-01-17 20:54:36 +0530

    query selector

--- dom.js
+++ dom.js
@@ -19,7 +19,7 @@ var header = document.getElementById('main-header');
 //console.log(headertitle);
 //headertitle.textContent='hello'
 //headertitle.innerText='b7y'
-header.style.borderBottom='solid 10px orange'
+header.style.borderBottom='solid 15px orange'
 var maintitle=document.getElementById('main');
 maintitle.style.color='blue'
 
@@ -32,3 +32,26 @@ items[3].style.fontWeight='Bold';
 
 
 items[2].style.backgroundColor='yellow';
+items[1].style.backgroundColor='green';
+
+
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 5px yellow'
+
+var item=document.querySelector('.list-group-item');
+item.style.color='red';
+
+var lastitem=document.querySelector('.list-group-item:last-child');
+lastitem.style.color='blue';
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color='orange';
+
+var thirdname=document.querySelectorAll('.list-group-item');
+console.log(thirdname);
+thirdname[2].textContent='     ';
+
+var odd=document.querySelectorAll('li:nth-Child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='orange'
+}

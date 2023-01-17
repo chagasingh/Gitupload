f783060fd88992192007ccc06ac70bd67642c733
chagan singh <chagansingh9876@gmail.com>
2023-01-17 14:02:52 +0530

    FormsJavascript

---
+++ dom.js
@@ -0,0 +1,24 @@
+
+
+//console.dir(document);
+
+//console.log(document.domain);
+//console.log(document.title);
+//document.title="GET A CALL"
+//console.log(document.head);
+//console.log(document.all);
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
+
+//get element by id
+
+//console.log(document.getElementById('exampleInputEmail1'))
+var headertitle=document.getElementById('header-title');
+var header = document.getElementById('main-header');
+//console.log(headertitle);
+//headertitle.textContent='hello'
+//headertitle.innerText='b7y'
+header.style.borderBottom='solid 10px orange'
+var maintitle=document.getElementById('main');
+maintitle.style.color='blue'
